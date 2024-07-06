d9d9054bc67999c947cb266d79b549556bedd0d4
brankokrstic94 <brankokrstic94@gmail.com>
2024-07-06 19:46:44 +0200

    some docs

--- counter/src/lib.rs
+++ counter/src/lib.rs
@@ -6,6 +6,10 @@ use std::{
 
 use proc_macro::TokenStream;
 
+// Experimental way I found to recreate the C __COUNTER__ preprocessor directive
+// Currently used to record performance of a specific code block to its respective slot in a buffer without runtime overhead of a table lookup
+// Macro relies on the existence of a build script which deletes the the temp file between compilation runs
+// It will likely fail miserably with incremental builds, and side-effects are not recommended inside proc macros, so this shouldn't be used anywhere else
 #[proc_macro]
 pub fn counter(_item: TokenStream) -> TokenStream {
     let out_dir = PathBuf::from(std::env::var("COUNTER_VALUE_FOLDER").unwrap());
